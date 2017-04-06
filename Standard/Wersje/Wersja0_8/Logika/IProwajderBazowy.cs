2a4672ea47aa213f8efcd856a29f528fe12dfc92
BudHub <michal.feliksik@uniterm24.pl>
2017-04-07 00:33:21 +0200

    prowajder bazowy

---
+++ Standard/Wersje/Wersja0_8/Logika/IProwajderBazowy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudHub.Standard.Wersje.Wersja0_8.Logika
+{
+    public interface IProwajderBazowy
+    {
+    }
+}
